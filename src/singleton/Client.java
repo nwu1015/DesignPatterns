c473914dc9442a85e6afc837a65b6a781dbd167b
nwu1015 <marchenkov3004@gmail.com>
2025-12-28 13:56:32 +0200

    singleton

---
+++ src/singleton/Client.java
@@ -0,0 +1,10 @@
+package singleton;
+
+public class Client {
+    public static void main(String[] args) {
+
+        //Singleton singleton = new Singleton();
+        System.out.println(Singleton.getSingleton());
+
+    }
+}
